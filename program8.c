6537e5e9ae0e8a46d03fd17bd6848e4ddd302b8e
priyankarajput-1BF24CS234 <priyankarajput.cs24@bmsce.ac.in>
2026-06-08 23:11:11 +0530

    Update program8.c

--- program8.c
+++ program8.c
@@ -2,65 +2,82 @@
 
 #define MAX 10
 
-int a[MAX][MAX], indegree[MAX], queue[MAX];
-int n;
+void topologicalSort(int n, int a[MAX][MAX])
+{
+    int indegree[MAX];
+    int queue[MAX];
 
-void topologicalSort() {
     int front = 0, rear = 0;
     int count = 0;
 
-    // Find indegree of each vertex
-    for (int i = 0; i < n; i++) {
+    // Calculate indegree of each vertex
+    for(int i = 0; i < n; i++)
+    {
         indegree[i] = 0;
-        for (int j = 0; j < n; j++) {
+
+        for(int j = 0; j < n; j++)
+        {
             indegree[i] += a[j][i];
         }
     }
 
     // Insert vertices with indegree 0 into queue
-    for (int i = 0; i < n; i++) {
-        if (indegree[i] == 0) {
+    for(int i = 0; i < n; i++)
+    {
+        if(indegree[i] == 0)
+        {
             queue[rear++] = i;
         }
     }
 
     printf("Topological Ordering: ");
 
-    while (front < rear) {
+    while(front < rear)
+    {
         int v = queue[front++];
         printf("%d ", v);
 
         count++;
 
-        for (int i = 0; i < n; i++) {
-            if (a[v][i] == 1) {
+        for(int i = 0; i < n; i++)
+        {
+            if(a[v][i] == 1)
+            {
                 indegree[i]--;
 
-                if (indegree[i] == 0) {
+                if(indegree[i] == 0)
+                {
                     queue[rear++] = i;
                 }
             }
         }
     }
 
-    if (count != n) {
+    if(count != n)
+    {
         printf("\nGraph contains a cycle");
     }
 }
 
-int main() {
+int main()
+{
+    int n;
+    int a[MAX][MAX];
+
     printf("Enter number of vertices: ");
     scanf("%d", &n);
 
     printf("Enter adjacency matrix:\n");
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
             scanf("%d", &a[i][j]);
         }
     }
 
-    topologicalSort();
+    topologicalSort(n, a);
 
     return 0;
 }

74499b283e4c34702ff49dd2c1b442c5a92d2f8e
priyankarajput-1BF24CS234 <priyankarajput.cs24@bmsce.ac.in>
2026-06-09 17:48:57 +0530

    Update p6.c

--- program6_floyds/p6.c
+++ program6_floyds/p6.c
@@ -1,63 +1,64 @@
 #include <stdio.h>
+#define INF 999
 
-#define INF 99999
+void readMatrix(int cost[10][10], int n)
+{
+    int i, j;
+    printf("Enter the cost matrix:\n");
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &cost[i][j]);
+        }
+    }
+}
 
-void printSolution(int n, int dist[n][n]) {
-    printf("\nShortest distances between every pair of vertices:\n");
+void floyd(int cost[10][10], int n)
+{
+    int i, j, k;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (dist[i][j] == INF)
-                printf("%7s", "INF");
-            else
-                printf("%7d", dist[i][j]);
+    for(k = 0; k < n; k++)
+    {
+        for(i = 0; i < n; i++)
+        {
+            for(j = 0; j < n; j++)
+            {
+                if(cost[i][j] > cost[i][k] + cost[k][j])
+                {
+                    cost[i][j] = cost[i][k] + cost[k][j];
+                }
+            }
+        }
+    }
+}
+
+void printMatrix(int cost[10][10], int n)
+{
+    int i, j;
+
+    printf("\nShortest path matrix:\n");
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            printf("%d ", cost[i][j]);
         }
         printf("\n");
     }
 }
 
-void floydWarshall(int n, int graph[n][n]) {
-    int dist[n][n];
-
-    // Initialize distance matrix
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            dist[i][j] = graph[i][j];
-
-    // Floyd-Warshall Algorithm
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dist[i][k] != INF && dist[k][j] != INF &&
-                    dist[i][k] + dist[k][j] < dist[i][j]) {
-
-                    dist[i][j] = dist[i][k] + dist[k][j];
-                }
-            }
-        }
-    }
-
-    printSolution(n, dist);
-}
-
-int main() {
+int main()
+{
     int n;
+    int cost[10][10];
 
     printf("Enter number of vertices: ");
     scanf("%d", &n);
 
-    int graph[n][n];
-
-    printf("Enter the adjacency matrix:\n");
-    printf("(Enter %d for no edge)\n", INF);
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &graph[i][j]);
-        }
-    }
-
-    floydWarshall(n, graph);
+    readMatrix(cost, n);
+    floyd(cost, n);
+    printMatrix(cost, n);
 
     return 0;
 }

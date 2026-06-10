4564824beea45a12ff96773499e5459122730b6f
priyankarajput-1BF24CS234 <priyankarajput.cs24@bmsce.ac.in>
2026-06-10 10:22:02 +0530

    Update code.c

--- program2(quick sort)/code.c
+++ program2(quick sort)/code.c
@@ -1,62 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
-int partition(int a[], int low, int high)
-{
-    int pivot = a[low];
-    int i = low + 1;
+int partition(int* arr, int low, int high){
+    int pivot = arr[low];
+    int i = low;
     int j = high;
-
-    while (i <= j)
-    {
-        while (i <= high && a[i] <= pivot)
+    while(i<j) {
+        while(arr[i] <= pivot) {
             i++;
-
-        while (a[j] > pivot)
+        }
+        while(arr[j]>pivot) {
             j--;
-
-        if (i < j)
-        {
-            int temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
+        }
+        if(i<j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
-
-    a[low] = a[j];
-    a[j] = pivot;
-
+    int temp = arr[low];
+    arr[low] = arr[j];
+    arr[j] = temp;
     return j;
 }
 
-void quicksort(int a[], int low, int high)
-{
-    if (low < high)
-    {
-        int j = partition(a, low, high);
-
-        quicksort(a, low, j - 1);
-        quicksort(a, j + 1, high);
+void quickSort(int*arr, int low, int high){
+    if(low<high){
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p-1);
+        quickSort(arr, p+1, high);
     }
 }
 
-int main()
-{
-    int n;
 
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
+int main() {
+    for(int i=1;i<=20;i++){
+        int n=10000*i;
+        int arr[n];
+        srand(time(NULL));
 
-    int a[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = rand();
 
-    printf("Enter elements:\n");
-    for (int i = 0; i < n; i++)
-        scanf("%d", &a[i]);
-
-    quicksort(a, 0, n - 1);
-
-    printf("Sorted array:\n");
-    for (int i = 0; i < n; i++)
-        printf("%d ", a[i]);
-
-    return 0;
+        clock_t start = clock();
+        quickSort(arr, 0, n - 1);
+        clock_t end = clock();
+        double cpu_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("%f\n",cpu_time);
+    }
 }
